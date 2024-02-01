ce561c18e1bc4be682b626b2a43ebebcaa7309eb
Jennifer-Paan <jenniferandpan@yahoo.com>
2024-02-01 18:37:50 +0200

    first submission

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.BmiTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BmiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Bmi()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Bmi(){
+    var heightInput:String by remember{ mutableStateOf("")}
+    var weightInput:String by remember{ mutableStateOf("")}
+    println("Recomposed: Height: $heightInput, Weight: $weightInput")
+
+    val  height = heightInput.toFloatOrNull()?:0.0f
+    val  weight = weightInput.toIntOrNull()?:0
+    println("Converted: Height: $height, Weight: $weight")
+
+    val bmi=if (weight>0 && height>0) weight/(height*height) else 0.0
+
+
+    Column(
+        modifier=Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.body_mass_index),
+            fontSize = 24.sp,
+            color=MaterialTheme.colorScheme.primary,
+            textAlign= TextAlign.Center,
+            modifier= Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
+            )
+        OutlinedTextField(
+            value = heightInput,
+            onValueChange ={heightInput=it.replace(',','.')},
+            label = { Text(text = stringResource(R.string.height))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = weightInput,
+            onValueChange ={weightInput=it.replace(',','.')},
+            label = { Text(text = stringResource(R.string.weight))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = stringResource(R.string.body_mass_index_is, String.format("%.2f", bmi).replace(',', '.')),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BmiTheme {
+        Bmi()
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/theme/BmiTheme.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BmiTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        content = content
+    )
+}
